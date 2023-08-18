8bcbbe667b5868c842fde64c3f51384e0b932699
Mahdi-Al-Sabea <mahdisabeh12345@gmail.com>
2023-08-18 16:48:37 +0300

    finished

--- birthday.js
+++ birthday.js
@@ -1,36 +1,60 @@
+
+let submit=document.querySelector('.submit');
+let userDate=document.querySelector('.user-date');
+let timeLeft=document.querySelector('.TimeLeft');
+let currentDate;
+let BirthDay;
+
+submit.addEventListener('click',(e)=>{
+
+    currentDate=new Date();
+    BirthDay=new Date(userDate.value);
+
+    if(userDate.value===''){
+        timeLeft.style.display="none";
+        alert('error you must enter a date first');
+    }
+    else if(BirthDay<currentDate){
+        timeLeft.style.display="none";
+        alert('error your date must be after the current day');
+    }
+    else{
+
+    timeLeft.style.display="block";
+    setInterval(TimeRemaining,1000);}
+
+});
+
+
+
 function TimeRemaining(){
 
 
+    currentDate=new Date();
 
-const currentDate=new Date();
-const BirthDay=new Date(2024,6,25,12,30,30);
+    let remainingTimeMilli=BirthDay-currentDate;//result is in milliseconds
+    let remainingTimeSeconds=Math.floor((BirthDay-currentDate)/1000);
+    let remainingTimeMinutes=Math.floor((BirthDay-currentDate)/(1000*60));
+    let remainingTimeHours=Math.floor((BirthDay-currentDate)/(1000*60*60));
+    let remainingTimeDays=Math.floor((BirthDay-currentDate)/(1000*60*60*24));
+    let remainingTimeMonth=getRemainingMonths(BirthDay,currentDate);
 
 
-let remainingTimeMilli=BirthDay-currentDate;
-let remainingTimeSeconds=Math.floor((BirthDay-currentDate)/1000);
-let remainingTimeMinutes=Math.floor((BirthDay-currentDate)/(1000*60));
-let remainingTimeHours=Math.floor((BirthDay-currentDate)/(1000*60*60));
-let remainingTimeDays=Math.floor((BirthDay-currentDate)/(1000*60*60*24));
-let remainingTimeMonth=getRemainingMonths(BirthDay,currentDate);
 
-console.log(remainingTimeMonth);
-
-let timeLeft=document.querySelector('.TimeLeft');
-timeLeft.innerText=`months left:${remainingTimeMonth}//remaining days left:${remainingTimeDays}//remaining hours left:${remainingTimeHours}//remaining minutes left:${remainingTimeMinutes}//remaining seconds left:${remainingTimeSeconds}`;
+    timeLeft.innerText=`months left:${remainingTimeMonth}//remaining days left:${remainingTimeDays}//remaining hours left:${remainingTimeHours}//remaining minutes left:${remainingTimeMinutes}//remaining seconds left:${remainingTimeSeconds}`;
 
 
 
 
-function getRemainingMonths(BirthDay,currentDate){
+    function getRemainingMonths(BirthDay,currentDate){//or we could use the upper method
 
-    let yearInterval=BirthDay.getFullYear()-currentDate.getFullYear();
-    
-    return yearInterval*12+BirthDay.getMonth()-currentDate.getMonth();
+        let yearInterval=BirthDay.getFullYear()-currentDate.getFullYear();
+        
+        return yearInterval*12+BirthDay.getMonth()-currentDate.getMonth();
 
 
-}
+    }
 
 }
 
 
-setInterval(TimeRemaining,1000);
